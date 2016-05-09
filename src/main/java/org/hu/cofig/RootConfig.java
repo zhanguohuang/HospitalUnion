95500d1775df324faaa3155fa1b3df98050e2cee
Administrator <Administrator@PC201605011359>
2016-05-09 23:00:20 +0800

    1

---
+++ src/main/java/org/hu/cofig/RootConfig.java
@@ -0,0 +1,11 @@
+package org.hu.cofig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages={""})
+public class RootConfig {
+	
+}
