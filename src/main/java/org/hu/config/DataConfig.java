5ee1feb51216b230607172c27a9a785778ab30bf
zhanguohuang <zhanguohuang@szhuarong.com>
2016-05-10 18:37:12 +0800

    1

--- src/main/java/org/hu/config/DataConfig.java
+++ src/main/java/org/hu/config/DataConfig.java
@@ -1,8 +1,25 @@
 package org.hu.config;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /*
  * DataSource bean
  */
+@Configuration
 public class DataConfig {
-
+	@Bean
+	public DataSource dataSource(){
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+		dataSource.setDriverClassName("com.mysql.jdbc.driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("mysqladmin");
+		dataSource.setInitialSize(20);
+		dataSource.setMaxActive(30);
+		return dataSource;
+	}
 }
--- src/main/java/org/hu/config/RootConfig.java
+++ src/main/java/org/hu/config/RootConfig.java
@@ -2,11 +2,13 @@ package org.hu.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /*
  * bean¹¤³§
  */
 @Configuration
+@Import(DataConfig.class)
 public class RootConfig {
 	
 }
