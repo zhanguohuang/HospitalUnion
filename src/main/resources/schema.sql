8f6d56e4530ed7f4accf3746bb55c0e2bdcd4a90
zhanguohuang <zhanguohuang@szhuarong.com>
2016-06-02 16:47:10 +0800

    1

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,7 +13,7 @@ create table limuseinf(
 	password varchar(50) not null,
 	role_user varchar(50),
 	primary key(username)
-)ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+);
 insert into user(id,username,password,email) values(1,'Õ²¹ú»Í','QQQwwe125','779134714@qq.com');
 insert into limuseinf(username,password,role_user) values('admin','1','ROLE_admin');
 
